1fe2fe986ba682357b2c81c977af3ddc5220c9bf
chantelletodd <150909661+chantelletodd@users.noreply.github.com>
2024-09-03 18:32:28 +0100

    Delete public directory

--- public/hamburger.js
+++
@@ -1,8 +0,0 @@
-
-function toggleNav(){
-    let links = document.querySelector(".mainNav");
-    links.classList.toggle('showNav');
-    }
-    
-    let hamburger = document.querySelector("#hamburger");
-    hamburger.addEventListener('click',toggleNav); 
